27192f9f13a621927f3c79e4bdbe0381c68a76f9
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-08-10 21:10:55 +0300

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n= sc.nextLong();
+        long m= sc.nextLong();
+        long a= sc.nextLong();
+        
+        System.out.println(  ( (n+a-1)/a ) *( (m+a-1)/a ) );
+    }
+}
